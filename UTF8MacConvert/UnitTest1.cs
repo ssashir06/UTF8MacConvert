2a60ad34fe17326870ce48e9df562a26d6c51b06
Hiroshi Takeda <takejaponpon+git@gmail.com>
2015-12-29 04:05:57 +0900

    change name of variables

--- UTF8MacConvert/UnitTest1.cs
+++ UTF8MacConvert/UnitTest1.cs
@@ -7,23 +7,23 @@ namespace UTF8MacConvert
     public class UnitTest1
     {
         // 44Gx44G044G344G644G944OR44OU44OX44Oa44Od
-        string utf8_nfc = "ぱぴぷぺぽパピプペポ"; // NFC
+        string utf8Nfc = "ぱぴぷぺぽパピプペポ"; // NFC
 
         // 44Gv44Ka44Gy44Ka44G144Ka44G444Ka44G744Ka44OP44Ka44OS44Ka44OV44Ka44OY44Ka44Ob44Ka
-        string utf8_nfd = "ぱぴぷぺぽパピプペポ"; //NFD
+        string utf8Nfd = "ぱぴぷぺぽパピプペポ"; //NFD
 
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreNotEqual(utf8_nfc, utf8_nfd);
-            Assert.AreEqual(false, utf8_nfd.IsNormalized());
+            Assert.AreNotEqual(utf8Nfc, utf8Nfd);
+            Assert.AreEqual(false, utf8Nfd.IsNormalized());
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var changed = utf8_nfd.Normalize();
-            Assert.AreEqual(utf8_nfc, changed);
+            var changed = utf8Nfd.Normalize();
+            Assert.AreEqual(utf8Nfc, changed);
             Assert.AreEqual(true, changed.IsNormalized());
         }
     }
